48bdcce13bfdcacd308d1c092e7583a075568bd8
JohnnyY8 <328965413@qq.com>
2018-07-21 10:41:50 +0800

    Add label for discriminator.

--- discriminator.py
+++ discriminator.py
@@ -16,16 +16,74 @@ class Discriminator(CommonModelFunc):
 
   # Get a graph for discriminator
   def getDiscriminator(self):
+    embeddingDimension = self.FLAGS.embeddingDimension
+    featureTypes = self.insDataPro.featureTypes
     num4FeatureTypes = self.insDataPro.num4FeatureTypes
 
     # ===== Discriminator layer =====
     with tf.variable_scope("discriminatorLayer"):
+      self.yLabel4Discriminator = tf.placeholder(
+          tf.float32,
+          [None, num4FeatureTypes],
+          name = "yLabel4Discriminator"]
+#      for featureType in featureTypes:
+#        if featureType == "DrugFingerPrint":
+#          # yLabel of discriminator for drug finger print
+#          self.yLabel4Discriminator4DrugFingerPrint = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4DrugFingerPrint")
+#
+#        elif featureType == "DrugPhy":
+#          # yLabel of discriminator for drug phy
+#          self.yLabel4Discriminator4DrugPhy = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4DrugPhy")
+#
+#        elif featureType == "L1000":
+#          # yLabel of discriminator for L1000
+#          self.yLabel4Discriminator4L1000 = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000")
+
+#          # yLabel of discriminator for L1000 A375
+#          self.yLabel4Discriminator4L1000A375 = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000A375")
+#
+#          # yLabel of discriminator for L1000 HA1E
+#          self.yLabel4Discriminator4L1000HA1E = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000HA1E")
+#
+#          # yLabel of discriminator for L1000 HT29
+#          self.yLabel4Discriminator4L1000HT29 = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000HT29")
+#
+#          # yLabel of discriminator for L1000 MCF7
+#          self.yLabel4Discriminator4L1000MCF7 = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000MCF7")
+#
+#          # yLabel of discriminator for L1000 PC3
+#          self.yLabel4Discriminator4L1000PC3 = tf.placeholder(
+#              tf.float32,
+#              [None, num4FeatureTypes],
+#              name = "yLabel4Discriminator4L1000PC3")
+
       name4W, name4B = "discriminatorW", "iscriminatorB"
       name4Z, name4H = "discriminatorZ", "discriminatorH"
 
       discriminatorW = self.init_weight_variable(
           name4W,
-          [self.insCNNModel.output4FixedSize4Discriminator
+          [embeddingDimension,
            num4FeatureTypes])
 
       discriminatorB = self.init_bias_variable(
@@ -34,7 +92,7 @@ class Discriminator(CommonModelFunc):
 
       self.discriminatorZ = tf.add(
           tf.matmul(
-              self.insCNNModel.output4FixedSize4Discriminator
+              self.insCNNModel.output4FixedSize4Discriminator,
               discriminatorW),
           discriminatorB,
           name = name4Z)
@@ -43,7 +101,6 @@ class Discriminator(CommonModelFunc):
           self.discriminatorZ,
           name = name4H)
 
-
     # ===== Loss layer for discriminator =====
     with tf.variable_scope("loss4DiscriminatorLayer"):
       name4Loss = "loss4Discriminator"
@@ -51,26 +108,24 @@ class Discriminator(CommonModelFunc):
       self.loss4Discriminator = tf.reduce_mean(
           tf.nn.softmax_cross_entropy_with_logits(
               logits = self.discriminatorZ,
-              labels = self.insCNNModel.yLabel4Discriminator),
+              labels = self.yLabel4Discriminator),
           name = name4Loss)
       #tf.summary.scalar("loss4Discriminator", tf.reduce_mean(self.loss4Discriminator))
 
       self.trainStep = tf.train.AdamOptimizer(
           self.FLAGS.learningRate).minimize(self.loss4Discriminator)
 
-
     # ===== Accuracy layer for discriminator =====
     with tf.variable_scope("accu4DiscriminatorLayer"):
       name4Accu = "accu4Discriminator"
 
       correctPrediction = tf.equal(
-          tf.argmax(self.discriminatorH, 1)
-          tf.argmax(self.insCNNModel.yLabel4Discriminator, 1))
+          tf.argmax(self.discriminatorH, 1),
+          tf.argmax(self.yLabel4Discriminator, 1))
 
       self.accu4Discriminator = tf.reduce_mean(
           tf.cast(correctPrediction, tf.float32),
           name = name4Accu)
       #tf.summary.scalar("accu4Discriminator", self.accu4Discriminator)
 
-    #self.merged = tf.summary.merge_all()
 
