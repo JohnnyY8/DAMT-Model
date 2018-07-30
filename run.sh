8c76449f5d062462dec8a1d9ee9a1576dae2dfd9
JohnnyY8 <328965413@qq.com>
2018-07-31 01:40:49 +0800

    Modify run.sh to old version.

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-#nohup python -u main.py --gpuId 0 --testSize 0.02 --num4Epoches 600 1>./files/info0.txt 2>&1 &
-#nohup python -u main.py --gpuId 1 --testSize 0.1 --num4Epoches 1000 1>./files/info1.txt 2>&1 &
-
-nohup python -u main.py 1>./files/info0.txt 2>&1 &
+nohup python -u main.py --gpuId 0 1>./files/info0.txt 2>&1 &
+nohup python -u main.py --gpuId 1 1>./files/info1.txt 2>&1 &
