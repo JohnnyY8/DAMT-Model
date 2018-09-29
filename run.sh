b84e99a0eb0be0d47f8a20652958c0c53b4f7eda
Lingwei Xie <johnny9141@hotmail.com>
2018-09-29 15:25:32 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 nohup python -u main.py --gpuId 0 1>./info0.txt 2>&1 &
-nohup python -u main.py --gpuId 1 1>./info1.txt 2>&1 &
+#nohup python -u main.py --gpuId 1 1>./info1.txt 2>&1 &
